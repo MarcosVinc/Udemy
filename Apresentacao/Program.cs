7ab23e1ef72591ea710f88ac1644ca0a1cf4307d
Marcos Vinicius <marcos.viniciuswac@outlook.com>
2021-02-26 03:08:42 -0300

    Atualizado 25/02/21

--- Apresentacao/Program.cs
+++ Apresentacao/Program.cs
@@ -6,72 +6,23 @@ namespace Apresentacao
     {
         static void Main(string[] args)
         {
-            Apresentacao();
-        }
-        public static void Apresentacao()
-        {
-            Console.WriteLine("|---------------------------------------------------|");
-            Console.WriteLine("| Dificuldades        Problemas         Facilidades |");
-            Console.WriteLine("|---------------------------------------------------|");
-            Console.WriteLine("| 1                       2                  3      |");
-            Console.WriteLine("|                                                   |");
-            Console.WriteLine("|- Precione a tecla referente a o que você deseja  -|");
-            Console.WriteLine("|---------------------------------------------------|");
-            int esc = 0;
-            if (int.TryParse(Console.ReadLine(), out esc))
-            {
-                switch (esc)
-                {
-                    case 1:
-                        Console.Clear();
-                        Dificuldades();
-                        break;
-                    case 2:
-                        Console.Clear();
-                        Problamas();
-                        break;
-                    case 3:
-                        Console.Clear();
-                        Facilidades();
-                        break;
+            /* 25/02/21 */
 
-                }
+            /*Dificuldades*/
+            /*for*/
 
+            /*Problamas*/
+            /*Tive que repostar todo o conteudo de uma vez*/
+            /*O repositorio ficou de mal e não deixava mas eu fazer nada*/
 
-            }
-
-            else
-            {
-                Console.WriteLine("Ação invalida, precione ENTER para voltar");
-                Console.ReadLine();
-                Console.Clear();
-                Apresentacao();
-            }
+            /*Facilidades*/
+            /*("if, else") 
+            ("while");
+            ("classe");
+            ("TryParse, e uma delicia fazer!");
+            */
 
         }
-
-        public static void Dificuldades()
-        {
-            Console.WriteLine("25/02/2021");
-            Console.WriteLine("for");
-        }
-        public static void Problamas()
-        {
-            Console.WriteLine(" 25/02/2021 ");
-            Console.WriteLine("Tive que repostar todo o conteudo de uma vez");
-            Console.WriteLine("O resitorio ficou de mal e não deixava mas eu fazer nada");
-
-
-        }
-        public static void Facilidades()
-        {
-            Console.WriteLine("25/02/2021");
-            Console.WriteLine("if, else");
-            Console.WriteLine("while");
-            Console.WriteLine("classe");
-            Console.WriteLine("TryParse, e uma delicia fazer!");
-
-
-        }
+       
     }
 }
