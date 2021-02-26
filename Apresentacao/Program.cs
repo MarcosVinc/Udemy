13af3faac7730661390708a890a1dfa598c41411
Marcos Vinicius <marcos.viniciuswac@outlook.com>
2021-02-26 02:39:58 -0300

    Me contrata

---
+++ Apresentacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Apresentacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
