9b0704603f724892aa122e98b0ef828b2d636b87
Marcos Vinicius <marcos.viniciuswac@outlook.com>
2021-02-26 02:46:25 -0300

    100% concluido

---
+++ ProvaPraticaDeLogicaDeProgramacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProvaPraticaDeLogicaDeProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
